53661ddbe75495cbc571d93c4e10d40360716a17
Young Hwan Shin <shinyh99@gmail.com>
2021-03-19 11:39:48 +0900

    Advanced course using car is done

--- my_car/scripts/cluster.py
+++ my_car/scripts/cluster.py
@@ -7,8 +7,12 @@ import enum
 import rospy
 import math
 from sensor_msgs.msg import LaserScan
-from my_car.msg import Array1d
-from my_car.msg import Array2d
+from my_car.msg import List_int
+from my_car.msg import Array_int
+
+# from geometry_msgs.msg import PoseArray
+
+from my_car.msg import List_position
 
 # from my_car.srv import srv_range
 
@@ -34,7 +38,8 @@ from my_car.msg import Array2d
 
 # raw_data = LaserScan()   # duck type un-neccesary
 # ranges = LaserScan().ranges
-CLUSTER = Array2d()
+CLUSTER = Array_int()
+POSITION = List_position()
 
 
 def collect_raw(input):
@@ -62,6 +67,10 @@ def cluster(ranges, angle_min, angle_max):
     # lateral, right
     array_y = [range_ * math.sin(angle) for range_, angle in zip(ranges, angles)]
 
+    global POSITION
+    POSITION.x = array_x
+    POSITION.y = array_y
+
     dist_square = []
     for i in range(length - 1):
         dist_square.append(
@@ -70,8 +79,8 @@ def cluster(ranges, angle_min, angle_max):
 
     # print(ranges)
     group = [[]]
-    ls = Array1d()
-    group = Array2d()
+    ls = List_int()
+    group = Array_int()
     group_i = 0
     threshold = 0.1  # if the distance btw the points are bigger than 0.1m, then consider them different object
     for i, length in enumerate(dist_square):
@@ -80,12 +89,13 @@ def cluster(ranges, angle_min, angle_max):
             # different group
             if length > threshold:
                 group_i += 1
-                group.int_2d.append(ls)
-                ls = Array1d()
+                if len(ls.elements) > 1:
+                    group.lists.append(ls)
+                ls = List_int()
             # same group
             else:
                 pass
-            ls.int_1d.append(i)
+            ls.elements.append(i)
     return group
 
 
@@ -93,14 +103,17 @@ def cluster(ranges, angle_min, angle_max):
 def run():
     rospy.init_node("cluster", anonymous=True)
     _ = rospy.Subscriber("carsim/laser/scan", LaserScan, collect_raw)
-    pub = rospy.Publisher("cluster", Array2d, queue_size=10)
-    global CLUSTER
+    pub_cluster = rospy.Publisher("cluster", Array_int, queue_size=10)
+    pub_position = rospy.Publisher("position", List_position, queue_size=10)
     rate = rospy.Rate(10)  # 10hz
+
+    global CLUSTER, POSITION
 
     while not rospy.is_shutdown():
         # rospy.loginfo("%f" % DISTANCE.front)
         # print(DISTANCE)
-        pub.publish(CLUSTER)
+        pub_cluster.publish(CLUSTER)
+        pub_position.publish(POSITION)
         rate.sleep()
 
     print("Ready to give range")
