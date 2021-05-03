8942982d6439d66d3ee732f79fab214cbcbeca4e
Young Hwan Shin <shinyh99@gmail.com>
2021-05-03 15:17:47 +0900

    Finished gmapping. let's start hector slam

---
+++ scout_mapper/readme.md
@@ -0,0 +1,7 @@
+# TF Tree
+
+### Map frame flow
+1. World
+2. Map
+3. Odom: odometry refence point
+4. Base link: scout location
--- scout_mapper/src/scout_mapper.py
+++ scout_mapper/src/scout_mapper.py
@@ -15,12 +15,13 @@ from tf.transformations import euler_from_quaternion, quaternion_from_euler
 import numpy as np
 from scipy.ndimage.interpolation import rotate
 
+
 class MapProc:
     def __init__(self):
         self.mapOrigCheck = False
-        self.mapOrigin = [0,0]
+        self.mapOrigin = [0, 0]
         self.mapHeading = 0
-        self.egoPos = [0,0]
+        self.egoPos = [0, 0]
         self.egoRot = Quaternion()
         self.egoHeading = 0
 
@@ -32,7 +33,9 @@ class MapProc:
     def callbackOccuGrid(self, msg):
         tmpPos = msg.info.origin.position
         tmpRot = msg.info.origin.orientation
-        _, _, tmpYaw = euler_from_quaternion([tmpRot.x, tmpRot.y, tmpRot.z, tmpRot.w])
+        _, _, tmpYaw = euler_from_quaternion(
+            [tmpRot.x, tmpRot.y, tmpRot.z, tmpRot.w]
+        )
         self.mapHeading = tmpYaw
         self.egoPos = [tmpPos.x, tmpPos.y]
         if not self.mapOrigCheck:
@@ -42,25 +45,32 @@ class MapProc:
         self.mapWidth = msg.info.width
         self.mapHeight = msg.info.height
         mapDataBuff = []
-        for i in range(0,40):
-            mapDataBuff.append(msg.data[i*40:i*40+39])
+        for i in range(0, 40):
+            mapDataBuff.append(msg.data[i * 40 : i * 40 + 39])
         self.mapData = msg
 
     def callbackImu(self, msg):
-        self.egoQuat = [msg.orientation.x, msg.orientation.y,
-                        msg.orientation.z, msg.orientation.w]
+        self.egoQuat = [
+            msg.orientation.x,
+            msg.orientation.y,
+            msg.orientation.z,
+            msg.orientation.w,
+        ]
         _, _, self.egoHeading = euler_from_quaternion(self.egoQuat)
-    
+
     def callbackOdom(self, msg):
         self.egoPos = [msg.pose.pose.position.x, msg.pose.pose.position.y]
         self.egoRot = msg.pose.pose.orientation
 
+
 if __name__ == "__main__":
-    rospy.init_node('scout_mapper', anonymous=False)
+    rospy.init_node("scout_mapper", anonymous=False)
     mapProc = MapProc()
-    rospy.Subscriber('OccupancyGrid', OccupancyGrid, mapProc.callbackOccuGrid, queue_size=1)
-    rospy.Subscriber('imu', Imu, mapProc.callbackImu, queue_size=1)
-    pubFixMap = rospy.Publisher('FixOccuGrid', OccupancyGrid, queue_size=1)
+    rospy.Subscriber(
+        "OccupancyGrid", OccupancyGrid, mapProc.callbackOccuGrid, queue_size=1
+    )
+    rospy.Subscriber("imu", Imu, mapProc.callbackImu, queue_size=1)
+    pubFixMap = rospy.Publisher("FixOccuGrid", OccupancyGrid, queue_size=1)
 
     rosrate = rospy.Rate(20)
     while not rospy.is_shutdown():
@@ -68,17 +78,23 @@ if __name__ == "__main__":
         tmpRes = tmpMap.info.resolution
         if tmpRes == 0:
             tmpRes = 1
-        tmpMap.info.origin.position.x -= tmpMap.info.width/(2/tmpRes)
-        tmpMap.info.origin.position.y -= tmpMap.info.height/(2/tmpRes)
-        
-        tmpMapData = np.reshape(tmpMap.data,
-                                (tmpMap.info.width, tmpMap.info.height))
-        tmpMapDataRot = rotate(tmpMapData,
-                               angle=mapProc.egoHeading/math.pi*180 + 180, reshape=False)
+        tmpMap.info.origin.position.x -= tmpMap.info.width / (2 / tmpRes)
+        tmpMap.info.origin.position.y -= tmpMap.info.height / (2 / tmpRes)
+
+        tmpMapData = np.reshape(
+            tmpMap.data, (tmpMap.info.width, tmpMap.info.height)
+        )
+        tmpMapDataRot = rotate(
+            tmpMapData,
+            angle=mapProc.egoHeading / math.pi * 180 + 180,
+            reshape=False,
+        )
         tmpMapDataRot = np.fliplr(tmpMapDataRot)
         tmpMapDataRot = tmpMapDataRot.astype(np.int8)
         tmpMapDataRot = np.clip(tmpMapDataRot, 0, 100)
-        tmpMapTot = np.reshape(tmpMapDataRot, (tmpMap.info.width * tmpMap.info.height))
+        tmpMapTot = np.reshape(
+            tmpMapDataRot, (tmpMap.info.width * tmpMap.info.height)
+        )
         tmpMap.data = tmpMapTot.tolist()
 
         tmpMapRot = quaternion_from_euler(0, 0, 0)
@@ -94,4 +110,3 @@ if __name__ == "__main__":
 
         rosrate.sleep()
     sys.exit(0)
-
